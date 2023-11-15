c9bcfbafe92e600260e2fb01e9258d7777bde9a2
eric <eric.jeangirard@recherche.gouv.fr>
2023-11-15 11:28:22 +0100

    normalize embeddings

--- project/server/main/scibert.py
+++ project/server/main/scibert.py
@@ -7,11 +7,13 @@ logger = get_logger(__name__)
 
 nlp = spacy.load("en_core_sci_scibert")
 
-def get_scibert_embeddings(text):
+def get_scibert_embeddings(text, normalize=True):
     if not isinstance(text, str) or text is None or len(text) == 0:
         return None
     doc = nlp(text)
-    tokvecs = doc._.trf_data.tensors[-1][0]
-    assert(len(tokvecs) == 768)
+    tokvecs = doc._.trf_data.tensors[-1]
+    if normalize:
+        tokvecs = preprocessing.normalize(tokvecs, norm='l2')
+    assert(len(tokvecs[0]) == 768)
     # converting to float for json serialization
-    return { 'embeddings': [float(e) for e in tokvecs] }
+    return { 'embeddings': [float(e) for e in tokvecs[0]] }
